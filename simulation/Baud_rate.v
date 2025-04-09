7471fd89ecd8713c0647c3258f862aa9db23b64d
Bùi Nguyễn Anh Duy <126902854+AnhDuy0106@users.noreply.github.com>
2025-04-09 14:13:19 +0700

    Create Baud_rate.v

---
+++ simulation/Baud_rate.v
@@ -0,0 +1,21 @@
+module Baud_rate (
+    input        i_Clock,
+    input        i_reset,
+    input        i_enable,
+    input  [7:0] brg_reg,
+    output wire  tick
+);
+
+    reg [4:0] bit_counter;
+
+    always @(posedge i_Clock or negedge i_reset) begin
+        if (i_reset)
+            bit_counter <= 0;
+      else if (tick) begin
+        bit_counter <= 0; end
+        	else 
+                bit_counter <= bit_counter + 1'b1;
+        end
+  assign tick = (bit_counter == (brg_reg + 1'b1)) ? 1'b1 : 1'b0;
+
+endmodule
